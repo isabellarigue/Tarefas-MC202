691bfce7067e83986e7a4d6e88c4f84398ffe9e7
Isabella Ribeiro Rigue <i234618@dac.unicamp.br>
2021-10-15 15:14:53 -0300

    adiciona comentarios no programa

--- tarefa12/busca.c
+++ tarefa12/busca.c
@@ -11,10 +11,12 @@ typedef struct No {
 
 typedef No * p_no;
 
+/* Cria uma árvore binária. */
 p_no criar_arvore() {
     return NULL;
 }
 
+/* Libera a memória alocada para uma árvore binária. */
 void destruir_arvore(p_no raiz) {
     if (raiz != NULL) {
         destruir_arvore(raiz->esq);
@@ -23,6 +25,7 @@ void destruir_arvore(p_no raiz) {
     }
 }
 
+/* Insere um anuncio na arvore binaria de busca organizada por valor. */
 p_no inserir_anuncio_por_valor(p_no raiz, char loja[], int codigo, double valor) { 
     p_no novo;
     if (raiz == NULL) {
@@ -40,6 +43,7 @@ p_no inserir_anuncio_por_valor(p_no raiz, char loja[], int codigo, double valor)
     return raiz;
 }
 
+/* Insere um anuncio na arvore binaria de busca organizada alfabeticamente pelo nome das lojas. */
 p_no inserir_anuncio_por_loja(p_no raiz, char loja[], int codigo, double valor) { 
     p_no novo;
     if (raiz == NULL) {
@@ -57,12 +61,15 @@ p_no inserir_anuncio_por_loja(p_no raiz, char loja[], int codigo, double valor) 
     return raiz;
 }
 
+/* Busca por anuncios na arvore cujo codigo do produto seja o mesmo e o valor não ultrapasse o max, 
+satisfeita essas condições o anuncio é adicionado em outra arvore binaria de busca organizada alfabeticamente. */
 p_no buscar_por_intervalo(p_no raiz, p_no lojas, double max, int codigo) {
     while (raiz != NULL) {
         if (raiz->valor <= max) { //esta no intervalo de preços
             if (raiz->codigo == codigo) //é o produto desejado
                 lojas = inserir_anuncio_por_loja(lojas, raiz->loja, raiz->codigo, raiz->valor);
-            lojas = buscar_por_intervalo(raiz->esq, lojas, max, codigo); //nesse caso os dois caminhos sao possiveis
+            //nesse caso os dois caminhos (esq, dir) sao possiveis para encontrar mais anuncios validos:
+            lojas = buscar_por_intervalo(raiz->esq, lojas, max, codigo); 
             raiz = raiz->dir;
         } else if (raiz->valor > max)
             raiz = raiz->esq;
@@ -72,24 +79,28 @@ p_no buscar_por_intervalo(p_no raiz, p_no lojas, double max, int codigo) {
     return lojas;
 }
 
+/* Substitui o elemento por seu sucessor e remove a antiga posição do sucessor, liberando memoria. */
 void remover_sucessor(p_no raiz) { 
-    p_no min = raiz->dir, pai = raiz; 
-    while (min->esq != NULL) { //buscando pelo minimo da subarvore esquerda e seu pai
-        pai = min;
-        min = min->esq;
+    p_no sucessor = raiz->dir, pai = raiz; 
+    //buscando pelo minimo da subarvore esquerda (sucessor) e seu pai:
+    while (sucessor->esq != NULL) { //o sucessor nunca tem filho esquerdo
+        pai = sucessor;
+        sucessor = sucessor->esq;
     }
-    if (pai->esq == min)
-        pai->esq = min->dir;
+    
+    if (pai->esq == sucessor)
+        pai->esq = sucessor->dir;
     else
-        pai->dir = min->dir;
+        pai->dir = sucessor->dir;
 
-    strcpy(raiz->loja, min->loja);
-    raiz->codigo = min->codigo;
-    raiz->valor = min->valor;
+    strcpy(raiz->loja, sucessor->loja);
+    raiz->codigo = sucessor->codigo;
+    raiz->valor = sucessor->valor;
 
-    free(min);
+    free(sucessor);
 }
 
+/* Remove um elemento de uma árvore binária de busca, liberando a memória. */
 p_no remover_rec(p_no raiz, char loja[], p_no ptr_removido) { 
     if (raiz == NULL)
         return NULL;
@@ -109,20 +120,22 @@ p_no remover_rec(p_no raiz, char loja[], p_no ptr_removido) {
     return raiz;
 }
 
+/* Retorna o minimo de uma arvore. */
 p_no minimo(p_no raiz) { 
     if (raiz == NULL || raiz->esq == NULL)
         return raiz;
     return minimo(raiz->esq);
 }
 
-void imprime_crescente(p_no raiz, int codigo) { //essa função imprime e libera
-    printf("OFERTAS para %d:\n", codigo);
+/* Imprime em ordem crescente (nesse caso alfabética) de acordo com o enunciado. 
+Essa função também libera a memoria da arvore, removendo cada elemento apos imprimir. */
+void imprime_crescente(p_no raiz) { 
     if (raiz != NULL) {
         p_no elemento = minimo(raiz);
         p_no ptr_removido = NULL;
         while (elemento != NULL) {
             printf("%s %d %.2lf\n", elemento->loja, elemento->codigo, elemento->valor);
-            raiz = remover_rec(raiz, elemento->loja, ptr_removido); //colocar uma função q ja usa o ponteiro pra otimizar?
+            raiz = remover_rec(raiz, elemento->loja, ptr_removido); 
             elemento = minimo(raiz);
         }
     } else 
@@ -132,28 +145,29 @@ void imprime_crescente(p_no raiz, int codigo) { //essa função imprime e libera
 }
 
 int main() {
-//buscar por preços q estejam no intervalo
-//para cada produto q achar, compara o codigo e ve se é o desejado
-//criar uma outra arvore para os produtos desejados e ir inserindo em ordem alfabetica
     int n, m, i, codigo;
     char loja[20];
     double valor;
     p_no anuncios = criar_arvore();
     p_no lojas;
 
+    //lendo os anuncios:
     scanf("%d", &n);
     for (i = 0; i < n; i++) {
         scanf("%s %d %lf", loja, &codigo, &valor);
         anuncios = inserir_anuncio_por_valor(anuncios, loja, codigo, valor);
     }
 
+    //lendo a consulta do usuario, buscando as ofertas e imprimindo-as:
     scanf("%d", &m);
     for (i = 0; i < m; i++) {
-        lojas = criar_arvore();
         scanf("%d %lf", &codigo, &valor);
+        lojas = criar_arvore();
         lojas = buscar_por_intervalo(anuncios, lojas, valor*1.1, codigo);
-        imprime_crescente(lojas, codigo);
+        printf("OFERTAS para %d:\n", codigo);
+        imprime_crescente(lojas);
     }
+
     destruir_arvore(anuncios);
 
     return 0;
