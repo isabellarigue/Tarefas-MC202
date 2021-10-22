619fa7d564174dd317b528e2c470879bcced6b58
Isabella Ribeiro Rigue <i234618@dac.unicamp.br>
2021-10-22 12:11:25 -0300

    adiciona comentarios

--- tarefa13/dns.c
+++ tarefa13/dns.c
@@ -5,7 +5,7 @@
 enum Cor {VERMELHO , PRETO};
 
 typedef struct No {
-    int acessos; //para o caso do usuario
+    int acessos; /* para o registro da quantidade de consultas do usuário */
     char dominio[101], endereco[16]; 
     enum Cor cor;
     struct No *esq, *dir;
@@ -27,18 +27,22 @@ void destruir_arvore(p_no raiz) {
     }
 }
 
+/* Retorna verdadeiro se a cor de x é VERMELHO. */
 int ehVermelho(p_no x) { 
     if (x == NULL)
         return 0;
     return x->cor == VERMELHO;
 }
 
+/* Retorna verdadeiro se a cor de x é PRETO. */
 int ehPreto(p_no x) {
     if (x == NULL)
         return 1;
     return x->cor == PRETO;
 }
 
+/* Faz uma rotação para a esquerda entre elementos, sem estragar as propriedades
+de busca e altura negra de uma arvore rubro negra esquerdista. */
 p_no rotaciona_para_esquerda(p_no raiz) { 
     p_no x = raiz->dir;
     raiz->dir = x->esq;
@@ -48,6 +52,8 @@ p_no rotaciona_para_esquerda(p_no raiz) {
     return x;
 }
 
+/* Faz uma rotação para a direita entre elementos, sem estragar as propriedades
+de busca e altura negra de uma arvore rubro negra esquerdista. */
 p_no rotaciona_para_direita(p_no raiz) { 
     p_no x = raiz->esq; 
     raiz->esq = x->dir;
@@ -57,13 +63,16 @@ p_no rotaciona_para_direita(p_no raiz) {
     return x;
 }
 
+/* Troca a cor do pai para VERMELHO e as cores dos filhos para PRETO, sem estragar
+a propriedade da altura negra de uma arvore rubro negra esquerdista. */
 void sobe_vermelho(p_no raiz) { 
     raiz->cor = VERMELHO;
     raiz->esq->cor = PRETO;
     raiz->dir->cor = PRETO;
 }
 
-//busca == 1 para dominio e busca == 0 para endereco
+/* Insere um elemento em uma ABB rubro negra esquerdista, mantendo todas as propriedades, 
+conforme código e funções auxiliares vistas durante a aula. */
 p_no inserir_rec(p_no raiz, char endereco[], char dominio[], int acessos, int busca) {
     if (raiz == NULL) {
         p_no novo = malloc(sizeof(No));
@@ -75,6 +84,7 @@ p_no inserir_rec(p_no raiz, char endereco[], char dominio[], int acessos, int bu
         return novo;
     }
 
+    /* Se busca igual a 1, a arvore é organizada de acordo com os dominios, caso contrario a arvore é organizada pelos enderecos. */
     if (busca == 1)  
         if (strcmp(dominio, raiz->dominio) < 0)
             raiz->esq = inserir_rec(raiz->esq, endereco, dominio, acessos, 1);
@@ -97,6 +107,7 @@ p_no inserir_rec(p_no raiz, char endereco[], char dominio[], int acessos, int bu
     return raiz;
 }
 
+/* Retorna o elemento desejado de uma ABB, organizada alfabeticamente pelo dominio. */
 p_no buscar_por_dominio(p_no raiz, char dominio[]) {
     if (raiz == NULL || strcmp(dominio, raiz->dominio) == 0)
         return raiz;
@@ -106,6 +117,7 @@ p_no buscar_por_dominio(p_no raiz, char dominio[]) {
         return buscar_por_dominio(raiz->dir, dominio);
 }
 
+/* Retorna o elemento desejado de uma ABB, organizada alfabeticamente pelo endereco. */
 p_no buscar_por_endereco(p_no raiz, char endereco[]) {
     if (raiz == NULL || strcmp(endereco, raiz->endereco) == 0)
         return raiz;
@@ -115,15 +127,9 @@ p_no buscar_por_endereco(p_no raiz, char endereco[]) {
         return buscar_por_endereco(raiz->dir, endereco);
 }
 
-int altura(p_no raiz) {
-    int h_esq, h_dir;
-    if (raiz == NULL)
-        return 0;
-    h_esq = altura(raiz->esq);
-    h_dir = altura(raiz->dir);
-    return 1 + (h_esq > h_dir ? h_esq : h_dir);
-}
-
+/* A ideia do codigo é utilizar ABBs balanceadas, no caso a escolhida foi a rubro negra esquerdista. Há uma 
+arvore para armazenar os servidores, que é organizada de acordo com os dominios, e uma arvore para os usuarios 
+(para ter controle do numero de consultas ja feitas), que é organizada de acordo com os endereços dos mesmos. */
 int main() {
     int u, n, m, i;
     char dominio[101], endereco[16], leitura[4], leitura1[5];
