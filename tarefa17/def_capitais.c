c1ec2a38f7a36a3d4969aa33629c6cd39d82687e
Isabella Ribeiro Rigue <i234618@dac.unicamp.br>
2021-11-22 21:29:32 -0300

    adiciona implementações e comentarios

--- tarefa17/def_capitais.c
+++ tarefa17/def_capitais.c
@@ -8,7 +8,7 @@
 
 typedef struct No {
     int populacao;
-    double peso;
+    double peso; //distancia entre duas cidades
     char nome[50];
     int vertice;
     double f_centralidade;
@@ -30,11 +30,12 @@ typedef Item * p_item;
 
 typedef struct {
     p_item *v;
-    int *indice; //indice[vetice_x] = ?
+    int *indice; //indice[vertice_x] = ?
     int n;
 } FP;
 typedef FP * p_fp;
 
+/* Cria e aloca memoria para um grafo. */
 p_grafo criar_grafo(int n) {
     p_grafo g = malloc(sizeof(Grafo));
     g->n = n;
@@ -42,6 +43,7 @@ p_grafo criar_grafo(int n) {
     return g;
 }
 
+/* Libera memoria alocada para uma lista ligada. */
 void libera_lista(p_no lista) {
     if (lista != NULL) {
         libera_lista(lista->prox);
@@ -49,6 +51,7 @@ void libera_lista(p_no lista) {
     }
 }
 
+/* Libera memoria alocada para um grafo. */
 void destroi_grafo(p_grafo g) {
     int i;
     for (i = 0; i < g->n; i++)
@@ -57,6 +60,7 @@ void destroi_grafo(p_grafo g) {
     free(g);
 } 
 
+/* Insere um p_no elemento, representando uma cidade, na lista ligada. */
 p_no insere_na_lista(p_no lista, char nome[], int populacao, int vertice, double distancia) { 
     p_no novo = malloc(sizeof(No));
     novo->populacao = populacao;
@@ -67,19 +71,21 @@ p_no insere_na_lista(p_no lista, char nome[], int populacao, int vertice, double
     return novo;
 }
 
+/* Insere uma aresta entre dois vertices (cidades) do grafo, sendo a distancia o peso da aresta. */
 void insere_aresta(p_grafo g, char cidade1[], char cidade2[], double distancia, p_no *cidades) { 
     int i, vertice1 = -1, vertice2 = -1;
     for (i = 0; (i < g->n) && (vertice1 == -1 || vertice2 == -1); i++) {
+        //procurando qual o numero do vertice correspondente a cada cidade
         if(strcmp(cidade1, cidades[i]->nome) == 0)
             vertice1 = i;
         if(strcmp(cidade2, cidades[i]->nome) == 0)
             vertice2 = i;
-    }
-        
+    }   
     g->adj[vertice1] = insere_na_lista(g->adj[vertice1], cidades[vertice2]->nome, cidades[vertice2]->populacao, cidades[vertice2]->vertice, distancia);
     g->adj[vertice2] = insere_na_lista(g->adj[vertice2], cidades[vertice1]->nome, cidades[vertice1]->populacao, cidades[vertice1]->vertice, distancia);
 }
 
+/* Cria e aloca memoria para uma fila de prioridade, no caso do programa sera um min heap. */
 p_fp criar_filaprio(int tam) {
     p_fp fprio = malloc(sizeof(FP));
     fprio->v = malloc(tam * sizeof(Item));
@@ -87,7 +93,7 @@ p_fp criar_filaprio(int tam) {
     return fprio;
 }
 
-/* Libera a memoria alocada para a estrutura. */
+/* Libera a memoria alocada para a fila. */
 void destruir_fila(p_fp fila, int n) {
     for (int i = 0; i < n; i++) 
         free(fila->v[i]);
@@ -95,9 +101,11 @@ void destruir_fila(p_fp fila, int n) {
     free(fila);
 }
 
-/* Troca a posição de duas cidades (Item) em um vetor. */
-void troca(p_item *a, p_item *b) {
+/* Troca a posição de duas cidades (p_item) em um vetor. Trocando também o indice de sua posição no vetor do heap. */
+void troca(p_item *a, p_item *b, p_fp h, int indice1, int indice2) {
     p_item aux = *a;
+    h->indice[(*a)->vertice] = indice2;
+    h->indice[(*b)->vertice] = indice1;
     *a = *b;
     *b = aux;
 }
@@ -105,7 +113,7 @@ void troca(p_item *a, p_item *b) {
 /* Sobe no heap, verificando e trocando as posições de alguns elementos para respeitar a condição de min heap, em que os filhos são maiores ou iguais ao pai. */
 void sobe_no_heap(p_fp fprio, int t) {
     if (t > 0 && fprio->v[PAI(t)]->prioridade > fprio->v[t]->prioridade) {
-        troca(&fprio->v[t], &fprio->v[PAI(t)]);
+        troca(&fprio->v[t], &fprio->v[PAI(t)], fprio, t, PAI(t));
         sobe_no_heap(fprio, PAI(t));
     }
 }
@@ -118,13 +126,13 @@ void desce_no_heap(p_fp fprio, int t) {
         if (F_DIR(t) < fprio->n && fprio->v[F_ESQ(t)]->prioridade > fprio->v[F_DIR(t)]->prioridade)
             menor_filho = F_DIR(t);
         if (fprio->v[t]->prioridade > fprio->v[menor_filho]->prioridade) {
-            troca(&fprio->v[t], &fprio->v[menor_filho]);
+            troca(&fprio->v[t], &fprio->v[menor_filho], fprio, t, menor_filho);
             desce_no_heap(fprio, menor_filho);
         }
     }
 }
 
-/* Insere em um heap. */
+/* Insere um p_item no heap/fprio. */
 void insere_heap(p_fp fprio, int vertice, double prioridade) { 
     p_item elemento = malloc(sizeof(Item));
     elemento->vertice = vertice;
@@ -138,7 +146,7 @@ void insere_heap(p_fp fprio, int vertice, double prioridade) {
 p_item extrai_minimo(p_fp fprio) {
     p_item elemento = fprio->v[0];
     if (fprio->n > 0) {    
-        troca(&fprio->v[0], &fprio->v[fprio->n - 1]);
+        troca(&fprio->v[0], &fprio->v[fprio->n - 1], fprio, 0, fprio->n - 1);
         fprio->n--;
         desce_no_heap(fprio, 0);
     } else 
@@ -152,11 +160,13 @@ void diminui_prioridade(p_fp fprio, int t, double valor) {
     sobe_no_heap(fprio, t);
 }
 
+/* Atualiza o vetor indice, que contém a posição correspondente a cada vertice do grafo no heap. */
 void atualiza_indice(p_fp h, int n) {
     for (int i = 0; i < n; i++) 
         h->indice[h->v[i]->vertice] = i;    
 }
 
+/* Devolve o vetor pai, que contem os "pais" de cada vertice, a partir dos quais é possível obter uma arvore de caminhos minimos. */
 int * dijkstra(p_grafo g, int s, p_no *cidades) {
     int i, *pai = malloc(g->n * sizeof(int));
     p_item v;
@@ -166,20 +176,16 @@ int * dijkstra(p_grafo g, int s, p_no *cidades) {
         pai[i] = -1;
         insere_heap(h, i, INT_MAX);
     }
-
     h->indice = malloc(g->n * sizeof(int));
-
     pai[s] = s;
     diminui_prioridade(h, s, 0);
-    atualiza_indice(h, g->n); //arrumar que isso vai deixar lerdo
+    atualiza_indice(h, g->n); 
     while (h->n > 0) { //heap nao vazio
         v = extrai_minimo(h);
-        atualiza_indice(h, g->n); 
         if (v->prioridade != INT_MAX)
             for (t = g->adj[v->vertice]; t != NULL; t = t->prox) {
                 if (v->prioridade + t->peso < h->v[h->indice[t->vertice]]->prioridade) {
                     diminui_prioridade(h, h->indice[t->vertice], v->prioridade + t->peso);
-                    atualiza_indice(h, g->n);
                     pai[t->vertice] = v->vertice; 
                 }
             }
@@ -189,7 +195,54 @@ int * dijkstra(p_grafo g, int s, p_no *cidades) {
     return pai;
 }
 
-/* Organiza o vetor decrescentemente de acordo com os valores de fator de centralidade. */
+/* Devolve a distancia gasta entre o vertice caminhos[w] e o vertice w. */
+double distancia_gasta(p_grafo grafo, int *caminhos, int w) {
+    double distancia = 0;
+    p_no vertice1 = grafo->adj[w];
+    while (vertice1->vertice != caminhos[w]) //procurando o vertice caminhos[w] na lista de adjacencias do vertice w
+        vertice1 = vertice1->prox;
+    distancia += vertice1->peso; 
+    return distancia;
+}
+
+/* Encontra os caminhos mínimos entre os vértices e a partir disso calcula o fator de centralidade de uma cidade/vertice. */
+void encontrar_caminhos_e_fcentralidade(p_grafo grafo, p_no *cidades, int populacao_total) {
+    int *caminhos, i, j, w, pop_atingida;
+    double distancia, f_centralidade;
+    for (i = 0; i < grafo->n; i++) { //para cada cidade do estado
+        caminhos = dijkstra(grafo, i, cidades);
+        f_centralidade = 0, pop_atingida = cidades[i]->populacao;
+        for (j = 0; j < grafo->n; j++) { //para cada outra cidade do estado
+            if (j != i) { //se a cidade for diferente dela mesma
+                w = j, distancia = 0;
+                /* A ideia aqui é a partir de um vertice_x ir indo ao pai (caminhos[w]) correspondente a cada vertice ate chegar em um pai que seja
+                a cidade (i) em questao (caminhos[w] == i), dessa forma estará percorrendo o caminho minimo entre vertice_x e a cidade/vertice i. */
+                while (caminhos[w] != i) {
+                    if (caminhos[w] == -1) //não ha caminho possivel entre as cidades
+                        break;
+                    else {
+                        distancia += distancia_gasta(grafo, caminhos, w);
+                        w = caminhos[w];   
+                    }
+                }
+                if (caminhos[w] != -1) {
+                    distancia += distancia_gasta(grafo, caminhos, w);
+                    pop_atingida += cidades[j]->populacao;
+                    f_centralidade += (distancia * cidades[j]->populacao);
+                }
+            }
+        }
+        f_centralidade = f_centralidade/pop_atingida;
+        if (pop_atingida > populacao_total/2) //verificar se é mais da metade da população do estado
+            cidades[i]->f_centralidade = f_centralidade;
+        else
+            cidades[i]->f_centralidade = 0;
+
+        free(caminhos);
+    }
+}
+
+/* Organiza o vetor crescentemente de acordo com os valores de fator de centralidade. */
 void merge_f(p_no *cidades, int l, int m, int r, int n) {
     p_no *aux = malloc(n * sizeof(No));
     int i = l, j = m + 1, k = 0;
@@ -213,8 +266,7 @@ void merge_f(p_no *cidades, int l, int m, int r, int n) {
         aux[k++] = cidades[j++];
     /*copia de volta para v*/
     for (i = l, k = 0; i <= r; i++, k++)
-        cidades[i] = aux[k];
-    
+        cidades[i] = aux[k]; 
     free(aux);
 }
 
@@ -238,65 +290,29 @@ void destruir_vetor(p_no *vetor, int max) {
 }
 
 int main() {
-    int n, i, j, w, populacao_total = 0, *caminhos, pop_atingida; 
-    double distancia, f_centralidade;
+    int n, i, populacao_total = 0; 
+    double distancia;
     char cidade1[50], cidade2[50];
 
     scanf("%d", &n);
     p_no *cidades = malloc(n * sizeof(No));
+    /* Cada cidade corresponde a um vertice do grafo, para identificar mais facilmente é criado esse vetor auxiliar em que cidades[vertice] = cidade.
+    Dessa forma, no vetor grafo->adj cada posição possui a lista das adjacencias de cada vertice, o qual pode ser encontrado no vetor cidades.  */
     for (i = 0; i < n; i++) {
         p_no cidade = malloc(sizeof(No));
         scanf("%s %d", cidade->nome, &cidade->populacao);
-        cidade->vertice = i; 
+        cidade->vertice = i;
         populacao_total += cidade->populacao;
         cidades[i] = cidade;
     }
 
     p_grafo grafo = criar_grafo(n);
-    while (scanf("%s %s %lf", cidade1, cidade2, &distancia) != EOF) {
+    while (scanf("%s %s %lf", cidade1, cidade2, &distancia) != EOF) 
         insere_aresta(grafo, cidade1, cidade2, distancia, cidades);
-    }
 
-    for (i = 0; i < n; i++) {
-        caminhos = dijkstra(grafo, i, cidades);
-        f_centralidade = 0;
-        pop_atingida = cidades[i]->populacao;
-        for (j = 0; j < n; j++) {
-            distancia = 0;
-            if (j == i)
-                continue;
-            else {
-                w = j;
-                while (caminhos[w] != i) {
-                    if (caminhos[w] == -1)
-                        break;
-                    else {
-                        p_no vertice1 = grafo->adj[w];
-                        while (vertice1->vertice != caminhos[w])
-                            vertice1 = vertice1->prox;
-                        distancia += vertice1->peso;
-                        w = caminhos[w];   
-                    }
-                }
-                if (caminhos[w] != -1) {
-                    p_no vertice1 = grafo->adj[w];
-                    while (vertice1->vertice != i) //fazer função
-                        vertice1 = vertice1->prox;
-                    distancia += vertice1->peso;
-                    pop_atingida += cidades[j]->populacao;
-                    f_centralidade += (distancia * cidades[j]->populacao);
-                }
-            }
-        }
-        f_centralidade = f_centralidade/pop_atingida;
-        if (pop_atingida > populacao_total/2) //verificar se é mais da metade da pop
-            cidades[i]->f_centralidade = f_centralidade;
-        else
-            cidades[i]->f_centralidade = 0;
-
-        free(caminhos);
-    }
+    encontrar_caminhos_e_fcentralidade(grafo, cidades, populacao_total);
     mergesort_f(cidades, 0, n - 1, n);
+
     for (i = 0; i < n; i++)
         if (cidades[i]->f_centralidade != 0)
             printf("%s %.2lf \n", cidades[i]->nome, cidades[i]->f_centralidade);
